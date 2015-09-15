a2114807f03e2a15ce5ee37ad7712fcaa3c0f147
RaulCos <cos_raul@yahoo.com>
2015-09-15 09:56:57 +0200

    multiplayer with beta importer
    v0.2

---
+++ Multiplayer with beta importer/Assets/Scripts/ButtonStare.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ButtonStare : MonoBehaviour
+{
+    public GameObject crosshairNear;
+    public GameObject crosshairFar;
+    public GameObject obj;
+    private RaycastHit hit;
+    private Ray ray;
+    // Use this for initialization
+    void Start ()
+    {
+        ray = new Ray();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("joystick 1 button 0") || Input.GetKeyDown(KeyCode.P))
+        {
+            ray.origin = crosshairNear.transform.position;
+            ray.direction = (crosshairFar.transform.position- crosshairNear.transform.position);
+            Physics.Raycast(ray,out hit,100);
+            Debug.DrawRay(ray.origin, ray.direction*100, Color.black,30);
+            obj = hit.transform.gameObject;
+            if (obj.GetComponent<Button>() != null)
+                {
+                obj.GetComponent<Button>().onClick.Invoke(); }
+        }
+        
+    }
+}
